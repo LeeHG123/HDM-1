5a6121665a08b19880528976e3fd8b47301a9b60
LeeHG123 <nsa15464@kaist.ac.kr>
2025-05-30 17:26:42 +0900

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,38 @@
+"""
+Some codes are partially adapted from
+https://github.com/AaltoML/generative-inverse-heat-dissipation/blob/main/scripts/datasets.py
+"""
+
+import numpy as np
+import torch
+
+from datasets.quadratic import QuadraticDataset
+from datasets.melbourne import MelbourneDataset
+from datasets.gridwatch import GridwatchDataset
+
+def data_scaler(data):
+    return data * 2. - 1.
+
+
+def data_inverse_scaler(data):
+    return (data + 1.) / 2.
+
+def get_dataset(config):
+    if config.data.dataset == "Quadratic":
+        dataset = QuadraticDataset(num_data=config.data.num_data,
+                                num_points=config.data.dimension,
+                                seed=42)
+        test_dataset = QuadraticDataset(num_data=config.data.num_data,
+                                        num_points=config.data.dimension,
+                                        seed=43)
+    elif config.data.dataset == "Melbourne":
+        dataset = MelbourneDataset(phase='train')
+        test_dataset = MelbourneDataset(phase='test')
+    elif config.data.dataset == "Gridwatch":
+        dataset = GridwatchDataset(phase='train', seed=87)
+        test_dataset = GridwatchDataset(phase='test', seed=85)
+    else:
+        raise NotImplementedError(f"Unknown dataset: {config.data.dataset}")
+    return dataset, test_dataset
+
+
